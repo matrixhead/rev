fd806a54a0cc50c412fb241336b591d8c77bc7e1
mhead <mtrxhead@protonmail.com>
2024-12-03 11:57:02 +0530

    removed unnecesary theming

--- lib/src/utils/theme/theme_builder.dart
+++ lib/src/utils/theme/theme_builder.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:rebar/src/utils/theme/color_consts.dart';
 
 ThemeData buildDefaultTheme() {
-  final ThemeData base = ThemeData.light();
-  return base.copyWith(
-    colorScheme: base.colorScheme.copyWith(
-        primary: primary, secondary: secondary, background: background),
-  );
+ return ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Color(0xfffd6671)),useMaterial3: true);
 }
